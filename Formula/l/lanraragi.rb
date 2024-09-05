e85e7fc23529e7da3b5683fd4a6d5db4ca7d55aa
Ilya Andreev <bugdealer@icloud.com>
2024-09-05 23:36:06 +0300

    lanraragi: use libarchive from homebrew

--- Formula/l/lanraragi.rb
+++ Formula/l/lanraragi.rb
@@ -6,16 +6,6 @@ class Lanraragi < Formula
   license "MIT"
   head "https://github.com/Difegue/LANraragi.git", branch: "dev"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "2a804967f1f97f008cf3d98c42a2e92204e3bedac38112a4e1c6ad9f3d228e60"
-    sha256 cellar: :any,                 arm64_ventura:  "ecec3c669e6bdb4e641961ff6a06e1d7e26d67c7d181e8118c89e2617355d1d5"
-    sha256 cellar: :any,                 arm64_monterey: "a1d3874f3e673370ccbd79e8364b046d98c6ed994836457355fb8f1df13f8c7a"
-    sha256 cellar: :any,                 sonoma:         "9780c6975588959ca66332bc34b25a2e8c51c023659cba40d7fbef9dc2a36ece"
-    sha256 cellar: :any,                 ventura:        "2f724d8529c7dbfd3bd770e229486d96817bd8eb11aa4ae943fa3be1fd5fda19"
-    sha256 cellar: :any,                 monterey:       "151a3319103602148c858f33b4a0d283af7eec42a9b56afc3e8d2785e65f4d77"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "34fa6e2e6af797dbdb0b6c1bc05e9b0349509827d7038430152b51e87b6cbd30"
-  end
-
   depends_on "nettle" => :build
   depends_on "pkg-config" => :build
 
@@ -24,6 +14,7 @@ class Lanraragi < Formula
   depends_on "giflib"
   depends_on "imagemagick"
   depends_on "jpeg-turbo"
+  depends_on "libarchive"  
   depends_on "libpng"
   depends_on "node"
   depends_on "openssl@3"
@@ -31,18 +22,10 @@ class Lanraragi < Formula
   depends_on "redis"
   depends_on "zstd"
 
-  uses_from_macos "libarchive"
   uses_from_macos "libffi"
 
   on_macos do
     depends_on "lz4"
-  end
-
-  resource "libarchive-headers" do
-    on_macos do
-      url "https://github.com/apple-oss-distributions/libarchive/archive/refs/tags/libarchive-131.tar.gz"
-      sha256 "8d0e4d71d2b039a968d2c7b4230806912785da98ce5d3a10c60024016ac343bb"
-    end
   end
 
   resource "Image::Magick" do
@@ -51,16 +34,10 @@ class Lanraragi < Formula
   end
 
   def install
-    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
-    ENV.prepend_path "PERL5LIB", libexec/"lib"
+    ENV.prepend_create_path "PERL5LIB", "#{libexec}/lib/perl5"
+    ENV.prepend_path "PERL5LIB", "#{libexec}/lib"
 
-    # On Linux, use the headers provided by the libarchive formula rather than the ones provided by Apple.
-    ENV["CFLAGS"] = if OS.mac?
-      "-I#{libexec}/include"
-    else
-      "-I#{Formula["libarchive"].opt_include}"
-    end
-
+    ENV["CFLAGS"] = "-I#{Formula["libarchive"].opt_include}"
     ENV["OPENSSL_PREFIX"] = Formula["openssl@3"].opt_prefix
 
     imagemagick = Formula["imagemagick"]
@@ -75,16 +52,8 @@ class Lanraragi < Formula
       system "make", "install"
     end
 
-    if OS.mac?
-      resource("libarchive-headers").stage do
-        cd "libarchive/libarchive" do
-          (libexec/"include").install "archive.h", "archive_entry.h"
-        end
-      end
-    end
-
     system "cpanm", "Config::AutoConf", "--notest", "-l", libexec
-    system "npm", "install", *std_npm_args(prefix: false)
+    system "npm", "install", *Language::Node.local_npm_install_args
     system "perl", "./tools/install.pl", "install-full"
 
     prefix.install "README.md"
@@ -94,13 +63,6 @@ class Lanraragi < Formula
       bin.install "lanraragi"
       libexec.install "redis.conf"
     end
-  end
-
-  def caveats
-    <<~EOS
-      Automatic thumbnail generation will not work properly on macOS < 10.15 due to the bundled Libarchive being too old.
-      Opening archives for reading will generate thumbnails normally.
-    EOS
   end
 
   test do
